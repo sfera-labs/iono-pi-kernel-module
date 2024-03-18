e92cc114c5f686f7f9ddd0e9a806a42207c842bc
Giampiero Baggiani <giampiero7@users.noreply.github.com>
2024-03-18 10:56:23 +0100

    Fix for RPi 3+

--- module.c
+++ module.c
@@ -36,7 +36,7 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sfera Labs - http://sferalabs.cc");
 MODULE_DESCRIPTION("Iono Pi driver module");
-MODULE_VERSION("1.20");
+MODULE_VERSION("1.21");
 
 #define LOG_TAG "ionopi: "
 
